9a49c4a2ad63c32c6588306590661e0d66ee74c2
00 <00>
2022-03-16 17:21:46 +0100

    Add project files.

---
+++ miljak1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace miljak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
